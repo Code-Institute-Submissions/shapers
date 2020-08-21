6f1e23104fbb4c9d5e864223117b136117ca45a7
Gareth <gareth.gs13@gmail.com>
2020-08-21 13:13:20 +0200

    heroku deployment config

--- shapers/settings.py
+++ shapers/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import mail_config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -22,14 +22,17 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DATABASE_URL', 'localdummysecretkey')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'localdummysecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUGGING')
 
 # If DEBUG, host is validated against ['localhost', '127.0.0.1', '[::1]']
 # if ALLOWED_HOST is left empty
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'test-shapers.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -122,12 +125,17 @@ WSGI_APPLICATION = 'shapers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -174,14 +182,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Email config
-if 'DEV' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'noreply@shapers.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = mail_config.user
-    EMAIL_HOST_PASSWORD = mail_config.password
-    DEFAULT_FROM_EMAIL = mail_config.user
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM_DEFAULT')
